da4b8b29c92f13be8f7cc523937812fc741d73b7
Mario <mariora.0013@gmail.com>
2025-10-17 13:57:10 +0200

    new(interfaces)_Lineales

---
+++ src/EEDD_Lineales/Iterador.java
@@ -0,0 +1,16 @@
+package EEDD_Lineales;
+
+public interface Iterador<D> {
+    // ZONA DE ATRIBUTOS
+    boolean hasNext();
+
+
+    // ZONA DE METODOS
+        // Constructor
+
+        // Métodos de la interfaz
+
+        // Métodos propios
+    D next();
+    void delete();
+}
